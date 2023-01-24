99dce6240320ae8e8d133e71e039b1fb4cd7c93c
Denys Volodarskyi <denys@volodarskyi.com>
2023-01-25 01:55:36 +0200

    ReadDelayLoadImports, DelayLoadImportedModule

--- Imports.cs
+++ Imports.cs
@@ -47,3 +47,26 @@ public class BoundImportInfo
     public string ModuleName { get; internal set; }
     public DateTime? TimeDateStamp { get; internal set; }
 }
+
+public class DelayLoadImportedModule
+{
+    /// <summary>
+    /// Name of the DLL to be loaded. 
+    /// </summary>
+    public string Name { get; internal set; }
+
+    /// <summary>
+    /// The timestamp of the DLL to which this image has been bound.
+    /// </summary>
+    public DateTime? TimeStamp { get; internal set; }
+
+    /// <summary>
+    /// The RVA of the module handle (in the data section of the image) of the DLL to be delay-loaded. 
+    /// It is used for storage by the routine that is supplied to manage delay-loading.
+    /// </summary>
+    public uint ModuleHandleRVA { get; internal set; }
+    
+    public List<ImportedSymbol> Symbols { get; internal set; }
+
+    public override string ToString() => $"{Name} ({Symbols.Count} import(s))";
+}
